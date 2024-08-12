1f5676d80f937e6a1e90293be8aa682157fa1e50
Hutch <tech_hutch@verizon.net>
2024-08-12 17:05:26 -0400

    add more basic setup

--- player/player.gd
+++ player/player.gd
@@ -31,21 +31,23 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if true:
-		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if is_on_floor() and not is_doing_action():
+		# Handle actions.
+		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
+		elif Input.is_action_just_pressed("ui_cancel"):
+			#animation_player.play("fire")
+			pass
 
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction := Input.get_axis("ui_left", "ui_right")
-		if direction:
-			sprite.flip_h = direction < 0
-			velocity.x = direction * SPEED
-			animation_player.play("walk")
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			animation_player.play("idle")
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction and not is_doing_action():
+		sprite.flip_h = direction < 0
+		velocity.x = direction * SPEED
+		animation_player.play("walk")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if not is_doing_action(): animation_player.play("idle")
 
 	move_and_slide()
 	
@@ -53,12 +55,14 @@ func _physics_process(delta: float) -> void:
 		die()
 		return
 	
-	if is_on_floor():
-		if Input.is_action_pressed("ui_cancel"):
-			#animation_player.play("fire")
-			pass
-	else:
+	if not (is_on_floor() or is_doing_action()):
 		animation_player.play("jump")
+
+
+func is_doing_action() -> bool:
+	var anim_name := animation_player.current_animation
+	#return anim_name == "fire"
+	return false
 
 
 func die() -> void:
